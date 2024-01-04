6c2cc681e56956e1a71ba6197f5cd6b87dff21e5
Nikoo <nikoomoallemi@gmail.com>
2024-01-04 21:11:22 +0330

    refactor commit

--- BMI.js
+++ BMI.js
@@ -1,17 +1,19 @@
 const hight = prompt('Enter Your Hight in Meters')
 const weight = prompt('Enter Your Weight in Kilograms')
+
 if(hight && weight){
-    const BMI = weight / (hight**2)
+    const BMI = Number(weight) / Number(hight ** 2);
     if(BMI < 18.5){
-        alert('Under weight')
-    }else if(BMI > 18.5 && weight < 24.9){
+        alert(' you are Underweight')
+    }else if(BMI > 18.5 && BMI < 24.9){
         alert('Normal')
-    }else if(BMI > 24.9 && weight < 29.9){
-        alert('Over weight')
+    }else if(BMI > 24.9 && BMI < 29.9){
+        alert(' You are Overweight')
     }else if(BMI > 29.9 ){
-        alert('your in danger area')
+        alert('you are in danger area')
     }else{
         alert('please enter your weight and hight properly')
     }
+}else{
+    alert('You have not entered anything. please reload the page and try again')
 }
-alert('You have not entered anything. please reload the page and try again')
