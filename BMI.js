e7b76af288c49d93cfa703959384de7016394fc5
Nikoo <nikoomoallemi@gmail.com>
2024-01-04 20:56:49 +0330

    initial commit

---
+++ BMI.js
@@ -0,0 +1,17 @@
+const hight = prompt('Enter Your Hight in Meters')
+const weight = prompt('Enter Your Weight in Kilograms')
+if(hight && weight){
+    const BMI = weight / (hight**2)
+    if(BMI < 18.5){
+        alert('Under weight')
+    }else if(BMI > 18.5 && weight < 24.9){
+        alert('Normal')
+    }else if(BMI > 24.9 && weight < 29.9){
+        alert('Over weight')
+    }else if(BMI > 29.9 ){
+        alert('your in danger area')
+    }else{
+        alert('please enter your age properly')
+    }
+}
+alert('You have not entered anything. please reload the page and try again')
---
+++ README.md
@@ -0,0 +1,3 @@
+# BMI Calculator
+
+asks you your weight and hight, and  calculates your BMI range, than tells you your bmi range
